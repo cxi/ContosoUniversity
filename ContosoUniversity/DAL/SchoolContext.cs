b9e4a6e1624fff2024cd912a242ac1be154013fd
Xi <charles.xi@bell.corp.bce.ca>
2017-02-10 15:25:56 -0500

    Add project files.

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; } //can be omitted since student references to Enrollment
+        public DbSet<Course> Courses { get; set; }  //can be omitted since Enrollment references to Course
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
